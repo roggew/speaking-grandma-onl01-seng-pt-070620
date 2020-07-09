bd9d7ba4d6e0941cb0d81fc17b921167ccc5e62d
William Rogge <williamroggeiv@gmail.com>
2020-07-09 02:11:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else phrase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
